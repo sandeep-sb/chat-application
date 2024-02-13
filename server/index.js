9b5d81179655caedf0477ab97eb6bf80f6cacc99
sandeep-sb <83261468+sandeep-sb@users.noreply.github.com>
2024-02-13 09:58:16 +0000

    socket logic added

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes")
 const messagesRoutes = require("./routes/messagesRoutes")
+const socket = require("socket.io");
 
 const app = express();
 require("dotenv").config();
@@ -27,4 +28,26 @@ mongoose.connect(process.env.MONGO_URL, {
 
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT}`);
+})
+
+const io = socket(server, {
+    cors: {
+        origin: "*",
+    }
+});
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket)=>{
+    global.chatSocket = socket;
+    socket.on("add-user", (userId)=>{
+        onlineUsers.set(userId, socket.id)
+    });
+
+    socket.on("send-msg", (data)=>{
+        const sendUserSocket = onlineUsers.get(data.to);
+        if(sendUserSocket){
+            socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+        }
+    })
 })
